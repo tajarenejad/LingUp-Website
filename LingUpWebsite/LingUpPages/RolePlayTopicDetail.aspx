2124f5df21edf9eecec6425aa17728dd343e2b0c
Zahra-Abaszadeh <133494154+Zahra-Abaszadeh@users.noreply.github.com>
2025-02-18 13:26:03 +0330

    refactor: apply BEM method to master page

---
+++ LingUpWebsite/LingUpPages/RolePlayTopicDetail.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/LingUpPages/LingUp.Master" AutoEventWireup="true" CodeBehind="RolePlayTopicDetail.aspx.cs" Inherits="LingUpWebsite.LingUpPages.RolePlayTopicDetail" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ LingUpWebsite/src/assets/js/script.js
@@ -0,0 +1,151 @@
+function initializeSidebarFix() {
+    const aside = document.querySelector(".layout__sidebar");
+    const container = document.querySelector(".layout__container");
+    const header = document.querySelector(".header__navbar");
+
+    function adjustasidePosition() {
+        if (!aside || !container || !header) return;
+
+        const headerHeight = header.offsetHeight;
+        const containerRect = container.getBoundingClientRect();
+        const asideHeight = aside.offsetHeight;
+        const windowHeight = window.innerHeight;
+
+        const containerStyles = window.getComputedStyle(container);
+        const containerPaddingRight = parseFloat(containerStyles.paddingRight);
+
+        const containerRight =
+            window.innerWidth - containerRect.right - containerPaddingRight;
+        if (
+            containerRect.top <= headerHeight &&
+            containerRect.bottom >= windowHeight
+        ) {
+            aside.style.position = "fixed";
+            aside.style.top = `${headerHeight}px`;
+            aside.style.removeProperty("bottom");
+            aside.style.right = `${containerRight}px`;
+            aside.style.removeProperty("left");
+        } else if (containerRect.bottom <= windowHeight) {
+            aside.style.position = "absolute";
+            aside.style.top = `${container.offsetHeight - asideHeight}px`;
+            aside.style.removeProperty("bottom");
+            aside.style.right = "0";
+            aside.style.removeProperty("left");
+        } else if (containerRect.top > headerHeight) {
+            aside.style.position = "fixed";
+            aside.style.top = `${headerHeight}px`;
+            aside.style.removeProperty("bottom");
+            aside.style.right = `${containerRight}px`;
+            aside.style.removeProperty("left");
+        } else {
+            aside.style.position = "static";
+            aside.style.removeProperty("top");
+            aside.style.removeProperty("bottom");
+            aside.style.removeProperty("right");
+            aside.style.removeProperty("left");
+        }
+    }
+
+    const mediaQuery = window.matchMedia("(min-width: 992px)");
+
+    function handleMediaQueryChange(e) {
+        if (e.matches) {
+            window.addEventListener("scroll", adjustasidePosition);
+            window.addEventListener("resize", adjustasidePosition);
+            document.addEventListener("DOMContentLoaded", adjustasidePosition);
+        } else {
+            window.removeEventListener("scroll", adjustasidePosition);
+            window.removeEventListener("resize", adjustasidePosition);
+            document.removeEventListener("DOMContentLoaded", adjustasidePosition);
+
+            if (aside) {
+                aside.style.position = "static";
+                aside.style.removeProperty("top");
+                aside.style.removeProperty("bottom");
+                aside.style.removeProperty("right");
+                aside.style.removeProperty("left");
+            }
+        }
+    }
+
+    handleMediaQueryChange(mediaQuery);
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+}
+if (document.querySelector(".layout__sidebar")) {
+    initializeSidebarFix();
+}
+
+// ===========================================
+
+function setEqualWidths() {
+    const participants = document.querySelectorAll(".conversation__participant");
+    let maxWidth = 0;
+
+    participants.forEach((participant) => {
+        maxWidth = Math.max(maxWidth, participant.offsetWidth);
+    });
+
+    participants.forEach((participant) => {
+        participant.style.width = maxWidth + "px";
+    });
+}
+
+window.addEventListener("load", setEqualWidths);
+window.addEventListener("resize", setEqualWidths);
+
+const recordButton = document.querySelector(".chat__record");
+const micCheckMessage = document.querySelector(".chat__mic-warning");
+const audioRecordWrapper = document.querySelector(".chat__audio-record");
+const spinnerWrapper = document.querySelector(".chat__spinner-container");
+const audioWave = document.querySelector(".chat__audio-wave");
+const audioList = document.querySelector(".audio-list");
+
+let mediaRecorder;
+let audioChunks = [];
+
+recordButton.addEventListener("click", async () => {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+        });
+        mediaRecorder = new MediaRecorder(stream);
+
+        mediaRecorder.ondataavailable = (event) => {
+            audioChunks.push(event.data);
+        };
+
+        mediaRecorder.onstop = () => {
+            const blob = new Blob(audioChunks, { type: "audio/wav" });
+            const audioURL = URL.createObjectURL(blob);
+
+            const audioElement = document.createElement("audio");
+            audioElement.src = audioURL;
+            audioElement.controls = true;
+
+            audioRecordWrapper.style.display = "none";
+            spinnerWrapper.style.display = "flex";
+
+            setTimeout(() => {
+                audioList.appendChild(audioElement);
+                spinnerWrapper.style.display = "none";
+                recordButton.style.display = "flex";
+                micCheckMessage.style.display = "none";
+                audioChunks = [];
+            }, 3000);
+        };
+
+        mediaRecorder.start();
+        recordButton.style.display = "none";
+        audioRecordWrapper.style.display = "flex";
+        micCheckMessage.style.display = "none";
+    } catch (error) {
+        console.error("Error accessing microphone:", error);
+        recordButton.style.display = "none";
+        micCheckMessage.style.display = "flex";
+    }
+});
+
+audioRecordWrapper.addEventListener("click", () => {
+    mediaRecorder.stop();
+});
